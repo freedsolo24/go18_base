d409b2d4155e4258539b97ae742471c5b3aff24c
freedsolo24 <freedsolo24@gmail.com>
2025-05-09 10:14:27 +0800

    ninemultiply

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go18_base
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func ninemultiply() {
+	var width int
+	for i := 1; i <= 9; i++ {
+		width = 2
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%dx%d=%-[4]*[3]d", j, i, i*j, width)
+			if j == 1 {
+				width++
+			}
+		}
+		fmt.Println()
+	}
+}
+func main() {
+	ninemultiply()
+}
