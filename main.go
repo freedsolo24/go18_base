b7eab37492771f927d3a7a669728a38fc09be837
freedsolo24 <freedsolo24@gmail.com>
2025-05-09 11:51:28 +0800

    v3

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import "fmt"
 
 func table9() {
 	var width int
+	/* for i:=1;i<=9;i++ {
+	      for j:=1;j<=9;j++ {
+	        if j==1 {
+	   	   width=2
+	   	   fmt.Printf("%dx%d=%-[4]*[3]d",j,i,i*j,width)
+	   	 } else {
+	   	   width=3
+	   	   fmt.Printf("%dx%d=%-[4]*[3]d",j,i,i*j,width)
+	   	 }
+	      }
+	   }
+	*/
 	for i := 1; i <= 9; i++ {
 		width = 2
 		for j := 1; j <= i; j++ {
