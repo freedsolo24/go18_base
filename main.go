d47845e15a336d8d28cb8b3932adbbab468f6041
freedsolo24 <freedsolo24@gmail.com>
2025-05-12 10:19:57 +0800

    new

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func table9() {
@@ -33,10 +34,14 @@ func table9() {
 
 func random_addmul() {
 
+	seed := time.Now().UnixNano() // 随机数种子
+	src := rand.NewSource(seed)   // 随机数发生源
+	r := rand.New(src)            // 随机数生成器
+
 	var sum int = 0
 	var mul uint64 = 1
 	for i := 0; i < 10; i++ {
-		random := rand.Intn(20) + 1
+		random := r.Intn(20) + 1
 		if random&1 == 1 {
 			sum += random // 任何数与1与，结果是1，是奇数
 		} else {
@@ -67,15 +72,35 @@ func fib_loop(n int) int {
 	return b // 最后return b，函数体消亡
 }
 
-func main() {
+func fib_rcrs(n int) int {
+	switch {
+	case n < 0:
+		panic("项数不能为0")
+	case n == 0:
+		return 0
+	case n < 3:
+		return 1
+	}
+	return fib_rcrs(n-1) + fib_rcrs(n-2)
+}
 
+func main() {
+	fmt.Println("==========九 九 乘 法 表==========")
 	table9()
-	fmt.Printf("++++++++++++\n")
+	fmt.Println("==========随机数累加累乘==========")
 	random_addmul()
-	fmt.Printf("++++++++++++\n")
+	fmt.Println("==========斐波那契额循环==========")
 	for i := 0; ; i++ {
-		if fib_loop(i) < 100 {
-			fmt.Printf("fib(%d)=%d\n", i, fib_loop(i))
+		if n := fib_loop(i); n < 100 {
+			fmt.Printf("fib(%d)=%d\n", i, n)
+		} else {
+			break
+		}
+	}
+	fmt.Println("==========斐波那契额递归==========")
+	for i := 0; ; i++ {
+		if n := fib_rcrs(i); n < 100 {
+			fmt.Printf("fib(%d)=%d\n", i, n)
 		} else {
 			break
 		}
